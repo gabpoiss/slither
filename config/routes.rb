e734130b19f5134a8d57261c6326f797f8b459c0
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-10 14:29:00 -0400

    fleshed out all routing and created all controller files (without methods).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get "/snakes", to: "snakes#index", as: "snakes"
-  get "/snakes/:snake_id", to: "snakes#show", as: "snake"
+  # get "/snakes", to: "snakes#index", as: "snakes"
+  # get "/snakes/:snake_id", to: "snakes#show", as: "snake"
+
+  resources :snakes do
+    resources :reviews, only: [:new, :create]
+    resources :bookings
+  end
+
+  get "/users/:user_id", to: "users#show", as: "user"
+  get "/users/:user_id/bookings", to: "bookings#user_show", as: "user_bookings"
+  get "/users/:user_id/snakes", to: "snakes#user_show", as: "user_snakes"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
