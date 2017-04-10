2a0bbbf7a98b88cb7b95f1aaed60206571a81ffb
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-10 17:21:49 -0400

    fixed small syntax bug

--- app/models/snake.rb
+++ app/models/snake.rb
@@ -1,9 +1,9 @@
 class Snake < ApplicationRecord
   belongs_to :user
   has_many :reviews, :bookings, dependent: :destroy
-  validates :name, presence: true, length: { in 6..20 }, exclusion: { in: (0..9).map! { |i| i.to_s } }
+  validates :name, presence: true, length: { in 6..20 }, exclusion: { in: (0..9)to_a.map! { |i| i.to_s } }
   validates :sex, presence: true, inclusion: { in ["male", "female", "unknown"]}
   validates :available, presence: true
-  validates :breed, presence: true, exclusion: { in: (0..9).map! { |i| i.to_s } }
+  validates :breed, presence: true, exclusion: { in: (0..9)to_a.map! { |i| i.to_s } }
   validates :price, presence: true, greater_than_or_equal_to: 0
 end
