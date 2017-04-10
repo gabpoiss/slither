095626999c56cb0990ac93c1231344f32eb682c4
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-10 17:14:12 -0400

    added validations from users, reviews, bookings, and snakes models. Some work still needs to be done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings, :reviews, :snakes, dependent: :destroy
+  validates :username, presence: true, length: { in: 6..20 }
+  validates :first_name, :last_name, presence: true, exclusion: { in: (0..9).map! { |i| i.to_s } }
+  validates :lat, :lng, presence: true
 end
