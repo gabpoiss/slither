47939385113c3e8deeddd2971ce22911e7c7b03d
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-12 15:54:48 -0400

    fixed bug on snake show (for real this time)

--- app/helpers/snakes_helper.rb
+++ app/helpers/snakes_helper.rb
@@ -2,10 +2,22 @@ module SnakesHelper
 
   def have_not_booked(snake)
     snakes_youve_booked = []
-    Booking.where(user_id: current_user.id).each do |i|
-      snakes_youve_booked << i.snake
+    bookings = Booking.where(user_id: current_user.id)
+    bookings.each do |i|
+      snakes_youve_booked << i.snake.id
     end
-    snakes_youve_booked.include? snake ? false : true
+
+    output = snakes_youve_booked.include?(snake.id) ? false : true
+    puts "Bookings:"
+    puts bookings
+    puts "Output:"
+    puts output
+    puts "Snake id"
+    puts snake.id
+    puts "ids of snakes you've booked"
+    print snakes_youve_booked
+
+    return output
   end
 
 end
