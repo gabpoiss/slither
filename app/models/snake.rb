39d03dad4e0d8fddc0df45b0c12b21db461675a2
Marc Brackett <admin@bookvetter.com>
2017-04-13 17:18:45 -0400

    final-search-Marc

--- app/models/snake.rb
+++ app/models/snake.rb
@@ -1,4 +1,5 @@
 class Snake < ApplicationRecord
+
   belongs_to :user
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
@@ -7,15 +8,9 @@ class Snake < ApplicationRecord
   validates :available, presence: true
   validates :breed, presence: true
   has_attachment :photo
+
+
   # We need a validator to make sure the price is greater than zero
   # validates :price, presence: true, greater_than_or_equal_to: 0
-
-def self.search(term)
-  if term
-    where('name LIKE ?', "%#{term}%")
-  else
-    all
-  end
-end
 
 end
