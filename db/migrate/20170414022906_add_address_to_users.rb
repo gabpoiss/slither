4539cb867567680a812d231ca4307dca01161355
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-14 14:17:48 -0400

    deleted redundant (hopefully) migrations

--- db/migrate/20170414022906_add_address_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :address, :string
-  end
-end
--- db/migrate/20170414030758_add_coordinates_to_snakes.rb
+++
@@ -1,7 +0,0 @@
-class AddCoordinatesToSnakes < ActiveRecord::Migration[5.0]
-  def change
-    # add_column :snakes, :address, :string
-    # add_column :snakes, :latitude, :float
-    # add_column :snakes, :longitude, :float
-  end
-end
